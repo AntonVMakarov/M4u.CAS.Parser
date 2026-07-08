dca7903eba5cf4f98916231a758703316d8aa8a7
AntonVMakarov <AntonVMakarov@hotmail.com>
2026-07-08 14:44:17 +0300

    Правки комментариев.

--- src/M4u.CAS.Parser/Tokenizer/Tokens/Token.cs
+++ src/M4u.CAS.Parser/Tokenizer/Tokens/Token.cs
@@ -2,9 +2,17 @@
 
 internal sealed record Token
 {
+    /// <summary>
+    /// Вид токена.
+    /// </summary>
     public TokenKind Kind { get; }
     
+
+    /// <summary>
+    /// Значение токена.
+    /// </summary>
     public string Value { get; }
+
 
     /// <summary>
     /// Структура, содержащая начальный индекс и длину токена в составе строки.
@@ -12,6 +20,12 @@ internal sealed record Token
     /// </summary>
     public TextSpan Span { get; }
 
+    /// <summary>
+    /// Открытый конструктор с параметрами.
+    /// </summary>
+    /// <param name="kind">Вид токена.</param>
+    /// <param name="value">Значение токена.</param>
+    /// <param name="span">Структура для определения местоположения токена в исходной строке.</param>
     public Token(TokenKind kind, string value, TextSpan span)
     {
         Kind = kind;
